fdc1920531ac830f3658de39cc8e32831ba241ea
aleesha <aleeshasj@gmail.com>
2026-03-15 00:28:05 +0530

    exported

---
+++ verify.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATA_DIR="/media/bmw/data/BEV/nuscenes/"
+
+MODEL_NAME="8x5_5e-4_rgb12_22:43:46"
+
+EXP_NAME="00" # evaluate rgb00 model
+
+python ./src/eval_nuscenes.py \
+       --batch_size=1 \
+       --exp_name=${EXP_NAME} \
+       --dset='mini' \
+       --data_dir=$DATA_DIR \
+       --log_dir='./artifacts/logs_eval_nuscenes' \
+       --init_dir="checkpoints/${MODEL_NAME}" \
+       --res_scale=2 \
+       --device_ids=[0] \
+       --export=False \
+       --onnx_runtime=True
+
+
+
